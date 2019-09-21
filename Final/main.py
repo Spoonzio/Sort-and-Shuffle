228c1f33f846b2556dcdf7b866cdbbcb240420a4
Spoonzio <jpan569@gmail.com>
2019-09-21 22:47:40 +0200

    Fixed animation in graph. Restricted bar count to 30

--- Final/main.py
+++ Final/main.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
 from PyQt5.uic import loadUi
 import random
 import time
@@ -10,9 +11,10 @@ import random
      
 class MatplotlibWidget(QMainWindow):
 
-    scrambled = None
+    # Declare class var
     xdata = None
     ydata = None
+    t = 0.01
 
     def __init__(self):
         # Constructor
@@ -31,8 +33,10 @@ class MatplotlibWidget(QMainWindow):
         # self.btn_Merge.clicked.connect()
         # self.btn_Quick.clicked.connect()
 
+        # Update Graph when spin-box is changed
         self.spnBars.valueChanged.connect(self.update_new_graph)
 
+        # Call scramble method when clicked
         self.btn_Scramble.clicked.connect(self.scramble_bars)
 
     def scramble_bars(self):
@@ -48,27 +52,29 @@ class MatplotlibWidget(QMainWindow):
             target = random.randint(j, len(scram_ys)-1)
             scram_ys[j] , scram_ys[target] = scram_ys[target], scram_ys[j]
         
-        self.scrambled = True
+        # Send scrambled data to class var
         self.ydata = scram_ys.copy()
         self.xdata = xs.copy()
 
-        print(self.xdata)
-        print(self.ydata)
-        self.draw_graph(xs, scram_ys)
+        # Draw new data onto graph
+        self.draw_graph(xs, scram_ys, None)
 
 
     def update_new_graph(self):
         # Update canvas on change event from the spin edit
         self.MplWidget.canvas.axes.clear()
 
+        # Create new dataset with changed size
         bar_count = self.spnBars.value()
         ys = [i for i in range(1, bar_count +1)]
         xs = ys.copy()
-        self.scrambled = False
+
+        # Send data to class var
         self.ydata = ys.copy()
         self.xdata = xs.copy()
 
-        self.draw_graph(xs,ys)
+        # Draw new data onto graph
+        self.draw_graph(xs, ys, None)
         
 
     def initial_graph(self):
@@ -76,37 +82,61 @@ class MatplotlibWidget(QMainWindow):
         self.update_new_graph()
 
 
-    def draw_graph(self, xs, ys):
+    def draw_graph(self, xs, ys, bar_color):
         # Draw graph from x-list and y-list
-        self.MplWidget.canvas.axes.bar(xs, ys, color = "#00A7E1")
+        if bar_color is None:
+            self.MplWidget.canvas.axes.bar(xs, ys, color = "#00A7E1")
+        else:
+            # Color parameter will highlight selected bar (Bar that is being moved)
+            self.MplWidget.canvas.axes.bar(xs, ys, color = bar_color)
+
         self.MplWidget.canvas.draw()
 
 
     def bubble_sort(self):
+        # Copy dataset
         yarray = self.ydata
-        for i in range(len(yarray)):
-            endp = len(yarray) - i
 
+        # Loop through all elements
+        for i in range(len(yarray)):
+            # Determine new endpoint as last i elements will be sorted (efficientcy)
+            endp = len(yarray) - i
+            
+            # Iterate over new resized dataset
             for j in range(0 , endp):
                 
+                # Prevent loop reaching out of list
                 if j+1 == len(yarray):
                     pass
                 else:
                     if yarray[j] > yarray[j+1]:
+
+                        # Swap elements if not ascending 
                         yarray[j], yarray[j+1] = yarray[j+1], yarray[j]
 
+                        # Update class var
                         self.ydata = yarray
-                        time.sleep(0.05)
+
+                        # Sleep to create a more pleasing animation
+                        # time.sleep(0.001)
                         self.MplWidget.canvas.axes.clear()
-                        self.draw_graph(self.xdata, self.ydata)
-                    
+
+                        # Create colour list to indicate which bar is highlighted
+                        bar_color = ["#00A7E1"] * (len(yarray)-1)
+                        bar_color.insert(j+1,"#ffa500")
+                        self.draw_graph(self.xdata, self.ydata, bar_color)
+
+                        # Process pending envents for the MPL graph
+                        QtCore.QCoreApplication.processEvents()
                     else:
+                        # Pass if 2 comparing data is in ascending order
                         pass
                     
                                
-    def swap(self, v1, v2):
-        return (v2, v1)
-
+    def ani_time(self):
+        # Determine sort wait time scaled to bars amount
+        if self.ydata:
+            return 1/(2*len(self.ydata))
 
 
 app = QApplication([])
